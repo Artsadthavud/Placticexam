c8af90ee6dba7dd0ce5fb75125d31dfbd26da620
Artsadthavud <56318713+Artsadthavud@users.noreply.github.com>
2019-10-09 21:56:49 +0700

    hw7

---
+++ ASASA.py
@@ -0,0 +1,24 @@
+
+x = input('Enter only 5 Arabic number: ')
+print("Thai number is ",end='')
+for i in range (0, 5) :
+    if x[i] == '1' :
+        print('๑',end="")
+    elif x[i] == '2' :
+        print("๒",end="")
+    elif x[i] == '3' :
+        print("๓",end="")
+    elif x[i] == '4' :
+        print("๔",end='')
+    elif x[i] == '5' :
+        print("๕",end='')
+    elif x[i] == '6' :
+        print("๖",end='')
+    elif x[i] == '7' :
+        print("๗",end='')
+    elif x[i] == '8' :
+        print("๘",end='')
+    elif x[i] == '9' :
+        print("๙",end='')
+    elif x[i] == '0' :
+        print("๐",end='')
